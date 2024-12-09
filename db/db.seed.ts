b740ab0385c7acfeef76598e4fbe0f1eb5ceace2
David <david.leon.m@gmail.com>
2024-12-09 12:19:38 +0100

    initialize

--- db/db.seed.ts
+++ db/db.seed.ts
@@ -1,23 +1,10 @@
 import { Logger } from '@nestjs/common'
 import { GlobalConfig } from '../src/config/global.config'
-import { Prisma, PrismaClient } from '@prisma/client'
-import { DefaultArgs } from '@prisma/client/runtime/library'
+import { Prisma } from '@prisma/client'
 import prisma from '../src/db/prismaClient.db'
 import * as bcrypt from 'bcrypt'
 
 const logger: Logger = new Logger('DB Initializer')
-type PrismaTransaction = Omit<
-  PrismaClient<
-    Prisma.PrismaClientOptions,
-    'log' extends keyof T
-      ? T['log'] extends Array<Prisma.LogLevel | Prisma.LogDefinition>
-        ? Prisma.GetEvents<T['log']>
-        : never
-      : never,
-    DefaultArgs
-  >,
-  '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
->
 
 /*
  * This data is used locally only for testing purposes.
@@ -34,13 +21,14 @@ main()
   })
 
 async function main(): Promise<void> {
-  await prisma.$transaction(async (tx: PrismaTransaction): Promise<void> => {
+  await prisma.$transaction(async (tx: Prisma.TransactionClient): Promise<void> => {
     await setUsers(tx)
-})}
+  })
+}
 
-async function setUsers(tx: PrismaTransaction): Promise<void> {
+async function setUsers(tx: Prisma.TransactionClient): Promise<void> {
   const passwordHashed: string = await bcrypt.hash('123456', GlobalConfig.auth.hashSaltRounds)
 
-  await tx.user.create({ data: { login:'dashboard', password: passwordHashed, role:'read' } })
-  await tx.user.create({ data: { login:'sensors', password: passwordHashed, role:'write' } })
+  await tx.user.create({ data: { login: 'dashboard', password: passwordHashed, role: 'read' } })
+  await tx.user.create({ data: { login: 'sensors', password: passwordHashed, role: 'write' } })
 }

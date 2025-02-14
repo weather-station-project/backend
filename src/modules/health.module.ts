389223929a0ce52b74715301baca3ba8f31234e1
David <david.leon.m@gmail.com>
2025-02-14 14:36:50 +0100

    no terminus

--- src/modules/health.module.ts
+++ src/modules/health.module.ts
@@ -1,14 +1,12 @@
 import { TerminusModule } from '@nestjs/terminus'
 import { Module } from '@nestjs/common'
 import { HealthController } from '../controllers/health.controller'
-import { GlobalConfig } from '../config/global.config'
-import { TerminusLogger } from '../utils/terminusLogger.util'
 
 @Module({
   imports: [
     TerminusModule.forRoot({
-      errorLogStyle: GlobalConfig.environment.isProduction ? 'json' : 'pretty',
-      logger: GlobalConfig.environment.isProduction ? TerminusLogger : true,
+      errorLogStyle: 'pretty',
+      logger: true,
     }),
   ],
   controllers: [HealthController],

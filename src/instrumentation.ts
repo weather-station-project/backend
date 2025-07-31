f3ba723e3ea8726cc20a5366efc3fe2ce544a79d
David <david.leon.m@gmail.com>
2025-07-31 14:09:08 +0200

    added http intrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,12 +2,13 @@ import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-tra
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import * as process from 'process'
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core'
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 
 const traceExporter = new ConsoleSpanExporter()
 
 export const otelSDK = new NodeSDK({
   spanProcessors: [new SimpleSpanProcessor(traceExporter)],
-  instrumentations: [new NestInstrumentation()],
+  instrumentations: [new HttpInstrumentation(), new NestInstrumentation()],
 })
 
 // gracefully shut down the SDK on process exit
